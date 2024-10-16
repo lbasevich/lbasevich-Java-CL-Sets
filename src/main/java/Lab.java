c3b931dfd7600a113ec0f6f79c9f8c1e57f7dcc6
Leo Basevich <lbasevich86@gmail.com>
2024-10-16 19:50:58 +0000

    Complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,4 +1,5 @@
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -19,7 +20,8 @@ public class Lab {
      * @return a new Set<Integer> object.
      */
     public Set<Integer> createSet(){
-        return null;
+        HashSet <Integer> result = new HashSet<Integer>();
+        return result;
     }
 
     /**
@@ -28,7 +30,7 @@ public class Lab {
      * @return the size of set. (number of values)
      */
     public int getSize(Set<Integer> set){
-        return 0;
+        return set.size();
     }
 
     /**
@@ -38,6 +40,9 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void addUniqueItem(Set<Integer> set, int item){
+        if (! set.contains(item)){
+            set.add(item);
+        }
 
     }
 
@@ -48,6 +53,7 @@ public class Lab {
      * @return nothing, pass by reference will cause changes to the list object to be reflected across the program.
      */
     public void removeItem(Set<Integer> set, int item){
+        set.remove(item);
 
     }
 }
